e779be068009332f70246312e437435cbee12475
Tim Duckett <tim@timduckett.co.uk>
2011-03-05 22:34:55 +0000

    Green spec tests for tweet times

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,21 @@
+# == Schema Information
+# Schema version: 20101122111011
+#
+# Table name: tweets
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  tweeted_at :datetime
+#  tweet_time :datetime
+#  repeat     :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Tweet < ActiveRecord::Base
     
     validates_size_of :content, :within => 1..140, :too_short => "should be at least one character!", :too_long => " can't be more than 140 characters long!"
-
+    
+    validates_date :tweet_time, :after => lambda { Date.current }
+        
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Tweet do
 
   before do
-    @tweet = Factory.create(:tweet)
+    @tweet = Factory.create(:tweet, :content => "Foo", :tweet_time => Time.utc(2011,12,1,10,11,12))
   end
 
   describe( "structure" ) do
@@ -14,8 +14,39 @@ describe Tweet do
     it { should have_a( :repeat ) }
   end
   
-  it "should not allow tweet content greater than 140 characters"
+  describe "tweet content" do
   
-  it "should not allow tweet content of less than 1 character"
+    it "should not allow tweet content greater than 140 characters" do
+    
+      long_content = "a" * 141
+    
+      long_content_tweet = Tweet.new(:content => long_content)
+      long_content_tweet.should_not be_valid
+
+    end
   
+    it "should not allow tweet content of less than 1 character" do
+      short_content_tweet = Tweet.new(:content => "")
+      short_content_tweet.should_not be_valid
+    end
+
+  end
+  
+  describe "times" do
+    
+    it "should not allow tweet times in the past" do
+      
+      past_time = Time.now - 1.day
+      past_tweet = Tweet.new(:content => "foo", :tweet_time => past_time)
+      past_tweet.should_not be_valid
+      
+    end
+    
+    it "should allow tweet times in the future" do
+      future_time = Time.now + 1.day
+      future_tweet = Tweet.new(:content => "foo", :tweet_time => future_time)
+      future_tweet.should be_valid
+    end
+    
+  end
 end
